2a71905a7fd81c2cec6faa38298b2fd1a4cf0d0d
Wizrd101 <simonhenderson100@gmail.com>
2023-01-18 07:47:16 -0800

    Pushing Project bc computer problems
    Technology is fun and I hate it

---
+++ Mobile Ex/Mobile Example Project/Assets/Scripts/MobileMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class MobileMovement : MonoBehaviour
+{
+    public int moveDir = 0;
+    public float moveSpeed = 1.0f;
+    public int mobileDir = 0;
+
+    public float jumpSpeed = 4.0f;
+    bool grounded = false;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveDir = 0;
+        int inputDir = (int)Input.GetAxisRaw("Horizontal");
+        moveDir += inputDir + mobileDir;
+        moveDir = Mathf.Clamp(moveDir, -1, 1);
+        Vector2 velocity = rb.velocity;
+        velocity.x = moveDir * moveSpeed;
+        rb.velocity = velocity;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    public void UpdateMoveDirection(int direction)
+    {
+        mobileDir = direction;
+    }
+
+    public void Jump()
+    {
+        if (grounded)
+        {
+            rb.AddForce(new Vector2(0, 100 * jumpSpeed));
+            grounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+}
